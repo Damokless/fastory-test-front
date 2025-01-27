3e3876c8ab8260258b6ebc7b80ebb47a46be1c34
damokless <damokless@protonmail.com>
2025-01-27 12:31:14 +0100

    feat : adding conditionnal rendering for presentations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+'use client'
+import Presentation from '@/components/presentation'
 import Search from '@/components/Search'
+import { useState } from 'react'
 
 export default function Home() {
+    const [selectionData, setSelectionData] = useState()
     return (
-        <div className='flex justify-center items-center w-screen'>
-            <Search />
+        <div className='flex justify-center items-center w-screen flex-col'>
+            <Search setValue={setSelectionData} />
+            { selectionData && <Presentation selectionData={selectionData} />}
         </div>
     )
 }
